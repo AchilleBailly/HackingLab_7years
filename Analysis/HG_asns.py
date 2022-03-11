6e7ece312a3709e18001a5ab21c7c7b9d999ee7a
Achille Bailly <achille.bailly@ensta-paris.fr>
2022-03-11 12:25:48 +0100

    Added description and asn_to_hg_kw function

--- Analysis/HG_asns.py
+++ Analysis/HG_asns.py
@@ -1,9 +1,14 @@
 import json
 from unicodedata import name
-from pexpect import EOF
 
 
-def get_HG_asns(input_file, output_file, HG_list):
+def HG_asns(input_file: str, output_file: str, HG_list: list(str)):
+    """
+        produce a .json file containing for each HG keyword in HG_list, their orgId and their ASNs
+        ARGS: input_file_name is the name of the CAIDA dataset file containing the worlds' ASNs
+              output_file_name is the name of the output file
+              HG_list is the list of the HG's keywords (eg. "GOOGLE" or "YAHOO")
+    """
     fulljson = dict()
     for HG in HG_list:
         HG_id = set()
@@ -36,5 +41,21 @@ def get_HG_asns(input_file, output_file, HG_list):
         json.dump(fulljson, file, indent=4)
 
 
+def get_HG_asns(input_file_name: str, save_file_name: str, HG_kw_list: list(str)):
+    """
+        opens save_file_name if it exists or creates it with function:HG_asns and returns json obj.
+        ARGS: save_file_name is the name of the CAIDA dataset file containing the worlds' ASNs
+              output_file_name is the name of the output file
+              HG_kw_list is the list of the HG's keywords (eg. "GOOGLE" or "YAHOO")
+    """
+    try:
+        with open(save_file_name, "rt") as file:
+            return json.load(file)
+    except:
+        HG_asns(input_file_name, save_file_name, HG_kw_list)
+        with open(save_file_name, "rt") as file:
+            return json.load(file)
+
+
 if __name__ == "__main__":
-    get_HG_asns("20220101.as-org2info.jsonl", "test.json", ["YAHOO", "GOOGLE"])
+    HG_asns("20220101.as-org2info.jsonl", "test.json", ["YAHOO", "GOOGLE"])
---
+++ Analysis/asn_to_HG_keyword.py
@@ -0,0 +1,38 @@
+import json
+
+
+def asn_to_kw(hg_asns_file: str, output_file_name: str):
+    """
+        creates a file mapping the asns to their HG kw
+        ARGS: hg_asns_file is the file containing the HGs asns
+              out_file_name is the name of the output file
+    """
+    with open(hg_asns_file, "rt") as file:
+        data = json.load(file)
+
+    out = dict()
+    for HG in data:
+        for asn in data[HG]["asns"]:
+            out[asn] = HG
+
+    with open(output_file_name, "wt") as output:
+        json.dump(out, output, indent=4)
+
+
+def get_asn_to_kw(hg_asns_file: str, output_file_name: str):
+    """
+        opens output_file_name if it exists or creates a file mapping the asns to their HG kw and returns it as json obj
+        ARGS: hg_asns_file is the file containing the HGs asns
+              out_file_name is the name of the output file
+    """
+    try:
+        with open(output_file_name, "rt") as file:
+            return json.load(file)
+    except:
+        asn_to_kw(hg_asns_file, output_file_name)
+        with open(output_file_name, "rt") as file:
+            return json.load(file)
+
+
+if __name__ == "__main__":
+    asn_to_kw("test.json", "asn_to_kw.json")
